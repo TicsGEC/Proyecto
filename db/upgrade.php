0f6da5abfab26e5a0e092d2543f3b5d277082779
pietroquercia <pietroquercia@192.168.0.5>
2015-06-13 01:21:03 -0300

    pull

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,6 +1,10 @@
  <?php
  function xmldb_local_proyecto_upgrade($oldversion) {
  	global $CFG, $DB;
+ 	
+ 	if ($oldversion < 2015061301){
+ 		  $field = new xmldb_field('nombredearchivo', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'seccion');
+ 	}
     if ($oldversion <2015061300 ) {
 
         // Define field id to be added to local_proyecto.
